04013cd662282c1ac62e2b92a8c8bdd59ab3dfbc
Michael Himing <mhiming@gmail.com>
2019-12-02 17:08:35 +1100

    Move python/01 to one file

--- python/01/part1.py
+++
@@ -1,2 +0,0 @@
-print(sum(int(x) // 3 - 2 for x in open("data.txt")))
-
--- python/01/part2.py
+++
@@ -1,9 +0,0 @@
-def cost(n):
-    result = 0
-    while n >= 9:
-        n = n // 3 - 2
-        result += n
-    return result
-
-
-print(sum(cost(int(x)) for x in open("data.txt")))
